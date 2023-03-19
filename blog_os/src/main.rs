5052a1b6e18c8867373f31f9c106cba00a405b18
kotabrog <suzuki.kota0331@gmail.com>
2023-03-19 14:41:59 +0900

    post 7

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -16,7 +16,8 @@ pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
 
-    loop {}
+    println!("It did not crash!");
+    blog_os::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -24,7 +25,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    blog_os::hlt_loop();
 }
 
 #[cfg(test)]
